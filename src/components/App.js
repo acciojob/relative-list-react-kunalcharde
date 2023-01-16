d40adbe606d5f671d30fe041e1ac837e30c119ba
Kunal Charde <kunalcharde1@gmail.com>
2023-01-17 00:09:41 +0530

    refractor

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
-import React ,{useState} from 'react'
+import React, { useState } from "react";
 
 const App = () => {
   // const relatives=["Mama", "chachu","bro","sis","uncle"]
-  const [relatives, setRelatives]=useState("Mama", "chachu","bro","sis","uncle")
+  const [relatives, setRelatives] = useState([
+    "Mama",
+    "chachu",
+    "bro",
+    "sis",
+    "uncle",
+  ]);
   return (
     <div id="main">
-          <ol>
-            {relatives.map((relative,idx)=>{
-              return <li key={relative+idx}>Name: {relative}</li>
-            })}
-          </ol>     
+      <ol key={"relativeList"}>
+        {relatives.map((relative, idx) => {
+          return <li key={`relativeListItem${idx}`}>Name: {relative}</li>;
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
